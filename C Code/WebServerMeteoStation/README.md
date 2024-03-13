a895a529167de495af20102e58ee58045c4d86dd
0nga <andreaongarini0@gmail.com>
2024-03-13 15:00:18 +0100

    changed from .txt file to .csv file

--- C Code/WebServerMeteoStation/README.md
+++ C Code/WebServerMeteoStation/README.md
@@ -1,11 +1,11 @@
 # Meteo station web server hosted by Raspberry pi
 
-Attempt to create the most basic web server (using Apache2) hosting a static html page containing info sent by some sensors.
+Attempt to create the most basic web server (using Apache2) hosting an html page containing info sent by some sensors.
 
-I have a C program that continuously catches sensor readings after a delay of 2 seconds, convert them into useful data and write them on a .txt file (data.txt).
+I have a C program that continuously catches sensor readings after a delay of 2 seconds, convert them into useful data and write them on a .csv file (data.csv).
 
-Then an html static page (that will be hosted by apache2) fetch data from data.txt file (must be in the same directory of index.html). This page reload itself every 2 seconds.
+Then an html page (in my case hosted by apache2) fetch data from data.csv file (must be in the same directory of index.html). This page show the last row of the file, the reload itself every 2 seconds and repeat the same thing.
 
-- [x] html static page that read .txt file and autoreload itself 
-- [x] C program that write results in a .txt file (random generated value that simulates sensor reading)
+- [x] html page that read .csv file and display the last row
+- [x] C program that write results in a .csv file (random generated value that simulates sensor reading)
 - [ ] DHT11 temperature and humidity sensor that sends data to some Raspberry pi pins
--- C Code/WebServerMeteoStation/writeTxtFile.c
+++ C Code/WebServerMeteoStation/writeTxtFile.c
@@ -6,22 +6,22 @@
 int main() {
     
     FILE *file;
-    char *filename = "./data.txt";
+    char *filename = "./data.csv";
     int sensorReading = 0;
 
+    // Attempt to delete the existing file
+    if (remove(filename) != 0) {
+	perror("Error deleting the file");
+    }
+    else {
+	printf("File deleted successfully\n");
+    }
+        
     for(;;){
 	sensorReading=rand()%10000;
 	
-	// Attempt to delete the existing file
-	if (remove(filename) != 0) {
-		perror("Error deleting the file");
-	}
-	else {
-		printf("File deleted successfully\n");
-	}
-        
-        // Open the file in "w" mode (override file content)
-        file = fopen(filename, "w");
+        // Open the file in "a" mode (append content to existing file)
+        file = fopen(filename, "a");
         
         if (file == NULL) {
             printf("Error opening file\n");
@@ -31,10 +31,9 @@ int main() {
 	    printf("File opened correctly\n");
 	}
 
-		
 	
-	fprintf(file, "Sensor Value: %d\n", sensorReading);
-	printf("Sensor Value: %d\n", sensorReading);
+	fprintf(file, "Temperature,%d\n", sensorReading);
+	printf("Temperature: %d\n", sensorReading);
 	
         
         // Flush the output buffer to ensure the data is written to the file immediately
@@ -42,7 +41,6 @@ int main() {
         printf("File flushed\n");
 
         // This is to prevent the loop from running too fast and overwhelming the system
-        // You can adjust this value based on your requirement
         sleep(1);
 
         // Close the file
